b636af3574f491436b4ec4b297fc2a04202badc6
Antonio Calvo <toniconmac@gmail.com>
2017-07-02 13:41:07 +0200

    Remove unnecessary queue from repository

--- RandomUser/UsersRepository.swift
+++ RandomUser/UsersRepository.swift
@@ -12,22 +12,16 @@ import Result
 class UsersRepository {
   fileprivate let randomUserAPIClient: APIClient
   
-  fileprivate let queue = DispatchQueue(label: "me.antoniocalvo.UsersRepository", qos: DispatchQoS.background)
-  
   init(randomUserAPIClient: APIClient) {
     self.randomUserAPIClient = randomUserAPIClient
-    
   }
   
   func getUsers(page: Int, results: Int, _ completion: @escaping (Result<[User], UsersError>) -> ()) {
     randomUserAPIClient.getUsers(results: results, page: page) { result in
-      self.queue.async {
-        completion(result.flatMap { (getUsersResult) -> Result<[User], APIClientError> in
-          return Result(value: getUsersResult.users)
-          
-          }.mapErrorToUserError())
-      }
+      completion(result.flatMap { (getUsersResult) -> Result<[User], APIClientError> in
+        return Result(value: getUsersResult.users)
+        
+        }.mapErrorToUserError())
     }
   }
-  
 }
